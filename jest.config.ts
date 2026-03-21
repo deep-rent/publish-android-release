413b3de58d63aa353c3816f2634b9bfebfa47337
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-03-21 21:01:59 +0100

    chore: hint coverage threshold config

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,14 @@ export default {
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
   coverageReporters: ['json-summary', 'text', 'lcov'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100
+  //   }
+  // },
   extensionsToTreatAsEsm: ['.ts'],
   moduleFileExtensions: ['ts', 'js'],
   preset: 'ts-jest',
