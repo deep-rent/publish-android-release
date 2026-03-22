4f3eac0d22e2d27b4c3ce6178d7f585f1ea8ea09
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-03-22 14:26:17 +0100

    build: minify javascript output

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
 const config = {
@@ -20,6 +21,12 @@ const config = {
       compilerOptions: {
         moduleResolution: 'bundler',
         module: 'ESNext',
+      },
+    }),
+    terser({
+      maxWorkers: 4,
+      compress: {
+        drop_console: false,
       },
     }),
   ],
