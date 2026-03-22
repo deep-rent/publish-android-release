f9375d7daa057bf8fb0b3bb56a287d0ef99e6de2
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-03-22 14:13:39 +0100

    build: register json plugin for rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
@@ -10,7 +11,12 @@ const config = {
     format: 'es',
     sourcemap: true,
   },
-  plugins: [nodeResolve({ preferBuiltins: true }), commonjs(), typescript()],
+  plugins: [
+    nodeResolve({ preferBuiltins: true }),
+    commonjs(),
+    json(),
+    typescript(),
+  ],
 }
 
 export default config
